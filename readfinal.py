3512f1eea1e80109592bca97fc66de354bb31cbf
EashwarK <168737876+EashwarK@users.noreply.github.com>
2024-05-03 11:09:57 +0530

    Update readfinal.py

--- readfinal.py
+++ readfinal.py
@@ -13,13 +13,13 @@ for i, word in enumerate(words):
     if word.upper() == "INTO":
         table_names.append(words[i + 1])
 print(table_names)
-for i in range(len(table_names)): 
-    filename = table_names[i]+".sql"
-    if os.path.exists(filename): 
-        with open(filename, "r+") as f:
-            content = f.read()
-            if sqlcmnd[i] not in content:
-                f.write(sqlcmnd[i]+'\n')
-    else:
-        with open(filename, "w") as f:
-            f.write(sqlcmnd[i]+'\n')
+# for i in range(len(table_names)): 
+#     filename = table_names[i]+".sql"
+#     if os.path.exists(filename): 
+#         with open(filename, "r+") as f:
+#             content = f.read()
+#             if sqlcmnd[i] not in content:
+#                 f.write(sqlcmnd[i]+'\n')
+#     else:
+#         with open(filename, "w") as f:
+#             f.write(sqlcmnd[i]+'\n')
